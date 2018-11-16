4490227401e91f13790a75cc202f6379b6c8486a
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-16 15:24:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves(array)
-  array.each_with_index do |item, index|
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item, index|
     puts "#{index + 1}. #{item}"
   end
 end
